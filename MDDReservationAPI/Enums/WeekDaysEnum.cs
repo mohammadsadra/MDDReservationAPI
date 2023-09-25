d5d50f7ce90fd3a859611bfeb8ba1d9595f0a254
Mohammad Sadra Haeri <43936083+mohammadsadra@users.noreply.github.com>
2023-09-25 10:33:57 +0330

    Added WeekDaysEnum.

---
+++ MDDReservationAPI/Enums/WeekDaysEnum.cs
@@ -0,0 +1,12 @@
+namespace MDDReservationAPI.Enums;
+
+public enum WeekDaysEnum
+{
+    Saturday = 1,
+    Sunday = 2,
+    Monday = 3,
+    Tuesday = 4,
+    Wednesday = 5,
+    Thursday = 6,
+    Friday = 7
+}
