b2253f3c60f3bf0e44672c69b70be106a742b291
Mohammad Sadra Haeri <43936083+mohammadsadra@users.noreply.github.com>
2023-12-20 15:26:05 +0330

    Comment unused apis.

--- MDDReservationAPI/Controllers/RegistrationFormController.cs
+++ MDDReservationAPI/Controllers/RegistrationFormController.cs
@@ -37,96 +37,98 @@ namespace MDDReservationAPI.Controllers
 
         #region POST
 
-        [HttpPost]
-        [Route("create")]
-        [Produces("application/json")]
-        public async Task<ActionResult<RegistrationForm>> CreateRegistrationFormAsync(
-            [FromBody] RegistrationFormCreationDTO registrationFormCreationDto)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
-            }
-
-            var createdRegistrationForm = _mapper.Map<RegistrationForm>(registrationFormCreationDto);
-
-            var result = await _reservationRepository.AddRegistrationFormAsync(createdRegistrationForm);
-
-            return Ok(result);
-        }
-
-        [HttpPost]
-        [Route("createFullForm")]
-        [Produces("application/json")]
-        public async Task<ActionResult<RegistrationForm>> CreateRegistrationFullFormAsync(
-            [FromBody] FullRegistrationFormDTO fullRegistrationFormDto)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
-            }
-
-            var manager = _mapper.Map<Manager>(fullRegistrationFormDto.managerCreationDto);
-            var school = _mapper.Map<School>(fullRegistrationFormDto.schoolCreationDto);
-            var schoolClass = _mapper.Map<SchoolClass>(fullRegistrationFormDto.schoolClassCreationDto);
-            var days = _mapper.Map<ReservationSelectedDay>(fullRegistrationFormDto.reservationSelectedDays);
-            var project = new Project();
-            if (fullRegistrationFormDto.hasProject)
-            {
-                project = _mapper.Map<Project>(fullRegistrationFormDto.projectCreationDto);
-            }
-
-
-            var createdManager = await _reservationRepository.AddManagerAsync(manager);
-            school.ManagerId = createdManager.Id;
-            var createdSchool = await _reservationRepository.AddSchoolAsync(school);
-            schoolClass.SchoolId = createdSchool.Id;
-            var createdClass = await _reservationRepository.AddSchoolClassAsync(schoolClass);
-            project.SchoolId = createdSchool.Id;
-            var createdProject = new Project();
-            if (fullRegistrationFormDto.hasProject)
-            {
-                createdProject = await _reservationRepository.AddProjectAsync(project);
-            }
-
-            var createdDay = await _reservationRepository.AddSelectedDays(days);
-
-            RegistrationForm form;
-            if (fullRegistrationFormDto.hasProject)
-            {
-                form = new RegistrationForm()
-                {
-                    ManagerId = createdManager.Id,
-                    SchoolId = createdSchool.Id,
-                    SchoolClassId = createdClass.Id,
-                    ReservationSelectedDaysId = createdDay.Id,
-                    ProjectId = createdProject.Id
-                };
-            }
-            else
-            {
-                form = new RegistrationForm()
-                {
-                    ManagerId = createdManager.Id,
-                    SchoolId = createdSchool.Id,
-                    SchoolClassId = createdClass.Id,
-                    ReservationSelectedDaysId = createdDay.Id,
-                };
-            }
-
-            _logger.LogError(fullRegistrationFormDto.ManagerFormFileId.ToString());
-            _logger.LogError(fullRegistrationFormDto.StudentListFileId.ToString());
-            
-            var createdForm = await _reservationRepository.AddRegistrationFormAsync(form);
-            await _reservationRepository.ChangeRegisterFormId(fullRegistrationFormDto.StudentListFileId,
-                createdForm.Id);
-            await _reservationRepository.ChangeRegisterFormId(fullRegistrationFormDto.ManagerFormFileId, createdForm.Id);
-            // var result = await _reservationRepository.CreatePdfFromRegistrationFormId(createdForm.Id);
-            var result = await _reservationRepository.CreateExcelFromRegistrationFormId(createdForm.Id);
-            // _mailService.Email(subject: "گزارش ثبت‌نام مدرسه", htmlString: result);
-            
-            return Ok("Successfully created.");
-        }
+        // [HttpPost]
+        // [Route("create")]
+        // [Produces("application/json")]
+        // public async Task<ActionResult<RegistrationForm>> CreateRegistrationFormAsync(
+        //     [FromBody] RegistrationFormCreationDTO registrationFormCreationDto)
+        // {
+        //     if (!ModelState.IsValid)
+        //     {
+        //         return BadRequest();
+        //     }
+        //
+        //     var createdRegistrationForm = _mapper.Map<RegistrationForm>(registrationFormCreationDto);
+        //
+        //     var result = await _reservationRepository.AddRegistrationFormAsync(createdRegistrationForm);
+        //
+        //     return Ok(result);
+        // }
+        //
+        // [HttpPost]
+        // [Route("createFullForm")]
+        // [Produces("application/json")]
+        // public async Task<ActionResult<RegistrationForm>> CreateRegistrationFullFormAsync(
+        //     [FromBody] FullRegistrationFormDTO fullRegistrationFormDto)
+        // {
+        //     if (!ModelState.IsValid)
+        //     {
+        //         return BadRequest();
+        //     }
+        //
+        //     var manager = _mapper.Map<Manager>(fullRegistrationFormDto.managerCreationDto);
+        //     var school = _mapper.Map<School>(fullRegistrationFormDto.schoolCreationDto);
+        //     var schoolClass = _mapper.Map<SchoolClass>(fullRegistrationFormDto.schoolClassCreationDto);
+        //     var days = _mapper.Map<ReservationSelectedDay>(fullRegistrationFormDto.reservationSelectedDays);
+        //     var project = new Project();
+        //     if (fullRegistrationFormDto.hasProject)
+        //     {
+        //         project = _mapper.Map<Project>(fullRegistrationFormDto.projectCreationDto);
+        //     }
+        //
+        //
+        //     var createdManager = await _reservationRepository.AddManagerAsync(manager);
+        //     school.ManagerId = createdManager.Id;
+        //     var createdSchool = await _reservationRepository.AddSchoolAsync(school);
+        //     schoolClass.SchoolId = createdSchool.Id;
+        //     var createdClass = await _reservationRepository.AddSchoolClassAsync(schoolClass);
+        //     project.SchoolId = createdSchool.Id;
+        //     var createdProject = new Project();
+        //     if (fullRegistrationFormDto.hasProject)
+        //     {
+        //         createdProject = await _reservationRepository.AddProjectAsync(project);
+        //     }
+        //
+        //     var createdDay = await _reservationRepository.AddSelectedDays(days);
+        //
+        //     RegistrationForm form;
+        //     if (fullRegistrationFormDto.hasProject)
+        //     {
+        //         form = new RegistrationForm()
+        //         {
+        //             ManagerId = createdManager.Id,
+        //             SchoolId = createdSchool.Id,
+        //             SchoolClassId = createdClass.Id,
+        //             ReservationSelectedDaysId = createdDay.Id,
+        //             ProjectId = createdProject.Id
+        //         };
+        //     }
+        //     else
+        //     {
+        //         form = new RegistrationForm()
+        //         {
+        //             ManagerId = createdManager.Id,
+        //             SchoolId = createdSchool.Id,
+        //             SchoolClassId = createdClass.Id,
+        //             ReservationSelectedDaysId = createdDay.Id,
+        //         };
+        //     }
+        //
+        //     _logger.LogError(fullRegistrationFormDto.ManagerFormFileId.ToString());
+        //     _logger.LogError(fullRegistrationFormDto.StudentListFileId.ToString());
+        //     
+        //     var createdForm = await _reservationRepository.AddRegistrationFormAsync(form);
+        //     await _reservationRepository.ChangeRegisterFormId(fullRegistrationFormDto.StudentListFileId,
+        //         createdForm.Id);
+        //     await _reservationRepository.ChangeRegisterFormId(fullRegistrationFormDto.ManagerFormFileId, createdForm.Id);
+        //     // var result = await _reservationRepository.CreatePdfFromRegistrationFormId(createdForm.Id);
+        //     var result = await _reservationRepository.CreateExcelFromRegistrationFormId(createdForm.Id);
+        //     // _mailService.Email(subject: "گزارش ثبت‌نام مدرسه", htmlString: result);
+        //     
+        //     return Ok("Successfully created.");
+        // }
+        //
+        
         
         [HttpPost]
         [Route("createFullFormWithFiles")]
@@ -208,7 +210,6 @@ namespace MDDReservationAPI.Controllers
             }
 
             var res =await _reservationRepository.CreateExcelFromRegistrationFormId(createdForm.Id);
-            _logger.LogError(res);
 
             // Check if file IDs are valid
             if (studentFileId == 0 || managerFileId == 0)
@@ -223,18 +224,6 @@ namespace MDDReservationAPI.Controllers
         #endregion
 
         #region GET
-
-        [HttpGet]
-        [Route("api/createReportPDF")]
-        public async Task<string> CreatePdfFromQuery(int id)
-        {
-            // Assume GetDataFromQuery is a method that executes SQL query and returns data
-
-            var result = await _reservationRepository.CreatePdfFromRegistrationFormId(id);
-            _mailService.Email(subject: "گزارش ثبت‌نام مدرسه", htmlString: result);
-
-            return result;
-        }
         
         [HttpGet]
         [Route("api/createReportExcel")]
@@ -253,6 +242,18 @@ namespace MDDReservationAPI.Controllers
             return result;
         }
         
+        
+        // [HttpGet]
+        // [Route("api/createReportPDF")]
+        // public async Task<string> CreatePdfFromQuery(int id)
+        // {
+        //     // Assume GetDataFromQuery is a method that executes SQL query and returns data
+        //
+        //     var result = await _reservationRepository.CreatePdfFromRegistrationFormId(id);
+        //     _mailService.Email(subject: "گزارش ثبت‌نام مدرسه", htmlString: result);
+        //
+        //     return result;
+        // }
         
         #endregion
         
